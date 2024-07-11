808777c3aed7c06995a7cb492bca7592ad1c6739
AramisJav <justinhereda@gmail.com>
2024-07-12 00:40:44 +0800

    .

--- Admin/admin2.js
+++ Admin/admin2.js
@@ -8,13 +8,18 @@ const menuList = document.querySelectorAll('.menuItem');
 menuList.forEach(menuItem => {
     // add event listener on each list element
     menuItem.addEventListener('click', () => {
-
         // removes all current active class
         menuList.forEach(listItem => {
             listItem.classList.remove('active');
         })
-        
         // add active class on selected list element
         menuItem.classList.add('active');
     })
+});
+
+// hide submenu on mouse leave
+const sidebar = document.getElementById('sidebar');
+sidebar.addEventListener('mouseleave', () => {
+    document.getElementById("maitenenceSubmenu").classList.add("hidden");
+    document.getElementById("maintenanceArrow").classList.remove("rotate-90");
 });
